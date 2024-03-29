90ff3ead4d01a9abd6509abfbe08147da79e6030
gikundiroHappy <gikundirohappy@gmail.com>
2024-03-29 10:21:06 +0200

    added two screens in the bottom nav

--- navigation/bottom.js
+++ navigation/bottom.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import {StyleSheet} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Feather from 'react-native-vector-icons/Feather'
+import Entypo from 'react-native-vector-icons/Entypo'
 import Home from '../screens/home';
 import Crud from '../screens/crud';
+import Images from '../screens/Images';
+import Task from '../screens/task';
 
 const Tab = createBottomTabNavigator();
 
@@ -38,6 +41,36 @@ export default function Bottomnav() {
             <Feather name="grid" size={25} color={focused? '#FFD130' : '#AFB2B1'}/>
          }} 
          />
+
+<Tab.Screen name="task" component={Task}
+        options={{ headerShown: false,
+          tabBarActiveTintColor: 'orange',
+          tabBarInactiveTintColor: '#AFB2B1',
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: '#1F2123',
+            borderTopColor: '#1F2123',
+          },
+          tabBarIcon: ({focused}) =>
+            <Feather name="shopping-cart" size={25} color={focused? '#FFD130' : '#AFB2B1'}/>
+         }} 
+         />
+
+<Tab.Screen name="images" component={Images} 
+        options={{ headerShown: false,
+          tabBarActiveTintColor: 'orange',
+          tabBarInactiveTintColor: '#AFB2B1',
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: '#1F2123',
+            borderTopColor: '#1F2123',
+          },
+          tabBarIcon: ({focused}) =>
+            <Entypo name="images" size={25} color={focused? '#FFD130' : '#AFB2B1'}/>
+         }} 
+         />
+
+
 
     </Tab.Navigator>
   );
